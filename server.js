8dadd36a61077c23eaf4d76d3002df9e14703ed9
Haden <haden@hadenfletcher.com>
2019-08-01 02:07:23 -0500

    forgot to commit and definitely will again

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const port = process.env['PORT'] || 8080
 
 const express = require('express')
+const path = require('path')
+
 const app = express()
+
 const utils = require('./utils')
+
+app.set('view engine', 'pug')
 
 app.use(require('body-parser').urlencoded({ extended: false }))
 app.use(require('body-parser').json())
 app.use(require('cookie-parser')())
 app.use(express.static('static'))
 
-app.set('view engine', 'pug')
+app.use('/css', require('postcss-middleware')({
+    src: (req) => {
+        const splitPath = req.path.split('/')
+        const file = splitPath.pop().split('.')
+        file.pop()
+        return path.join(__dirname, 'static', 'css', splitPath.join('/'), `${file}.pcss`)
+    },
+    plugins: [
+        require('tailwindcss')('tailwind.config.js'),
+        require('autoprefixer')(),
+        require('postcss-import')(),
+        require('postcss-nested')()
+    ]
+}))
 
 app.get('*', (req, res, next) => {
     console.log(`Requested ${req.path}...`)
@@ -19,12 +37,12 @@ app.get('*', (req, res, next) => {
 
 app.get('/', function (req, res) {
     if (req.cookies['userAuth'])
-        res.redirect('/b/accounts')
-    else
-        res.render('landing', {
-            query: req.query,
-            ...utils.basicData(req)
-        })
+        return res.redirect('/b/accounts')
+
+    res.render('landing', {
+        query: req.query,
+        ...utils.basicData(req)
+    })
 })
 
 app.use('/admin', require('./apps/admin'))
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    important: true,
+    theme: {
+        fontFamily: {
+            display: ['Cerebri Sans', 'Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif'],
+            body: ['Segoe UI', 'Tahoma', 'Geneva', 'Verdana', 'sans-serif']
+        },
+        extend: {
+            colors: {
+                inherit: 'inherit',
+                primary: {
+                    default: '#1315ab',
+                    100: '#7172cc',
+                    200: '#2426e7',
+                    300: '#181bd9',
+                    400: '#1618c2',
+                    500: '#1315ab',
+                    600: '#101294',
+                    700: '#0e0f7d',
+                    800: '#0b0d66',
+                    900: '#0b0c66',
+                }
+            }
+        }
+    },
+    variants: {
+
+    },
+}
