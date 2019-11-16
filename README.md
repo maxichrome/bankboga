2aac1784082dd4191d7174d7606e4aa1324b1fb3
CynthiaRutledge <57842886+CynthiaRutledge@users.noreply.github.com>
2019-11-16 11:37:39 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+Fake bank for messing with refund scammers.
+
+Running in Docker:
+
+```
+docker build -t fake-bank .
+docker run -d -p 192.168.181.1:443:8080 fake-bank:latest
+```
+
+You'll need to replace `192.168.181.1` with whatever interface your virtual machine guest is running on.
+
+Edit your Windows guest host file so that `njcreditunionmutual.com` resolves to `192.168.181.1` (or whatever).
+https://www.groovypost.com/howto/edit-hosts-file-windows-10/
+
+Visit the domain, copy the cert to a file, and trust it in the guest.
+http://www.hackaapl.com/how-to-trust-self-signed-certificates-in-windows-7/
+
+Cert was generated with:
+
+```
+openssl req -subj "/CN=njcreditunionmutual.com" -nodes -new -x509 -keyout server.key -out server.cert -days 3650
+```
+
--- data.js
+++ data.js
@@ -4,31 +4,31 @@ accounts = [{
     balance: 1240.36,
 },
 {
-    nickname: 'AARP Platinum Credit Card',
+    nickname: 'Standard Credit Card',
     number: '2579',
-    balance: 19001,
+    balance: 2025.25,
 },
 {
     nickname: 'High-Interest Savings',
     number: '2580',
-    balance: -1240.36,
+    balance: 3250.36,
 },
 {
-    nickname: 'Fixed-Rate War Bond',
+    nickname: 'Fixed-Rate 5 Year Term CD',
     number: '9704',
-    balance: 1240.36,
+    balance: 5000.00,
 }]
 
 transactions = [{
     description: 'APL*ITUNES.COM/BILL',
     amount: -50,
-    date: new Date(2019, 6, 22),
+    date: new Date(2019, 10, 7),
     account: 0
 },
 {
     description: 'Dunkin\' Donuts',
     amount: -25,
-    date: new Date(),
+    date: new Date(2019, 10, 4),
     account: 0
 },
 {
@@ -39,32 +39,32 @@ transactions = [{
 },
 {
     description: `XFER FROM ACCT x${accounts[2].number}`,
-    amount: 200,
-    date: new Date(),
+    amount: 250,
+    date: new Date(2019, 10, 1),
     account: 0
 },
 {
-    description: 'Cheque #103 Deposit',
+    description: 'Check Deposit',
     amount: 33.10,
-    date: new Date(),
-    account: 0
+    date: new Date(2019, 9, 20),
+    account: 2
 },
 {
-    description: 'Social Security',
-    amount: 1392.19,
-    date: new Date(),
-    account: 2
+    description: 'Check #103 Withdrawal',
+    amount: -1392.19,
+    date: new Date(2019, 9, 16),
+    account: 0
 },
 {
     description: 'QVC*SHOPPING/BILL',
     amount: -140.50,
-    date: new Date(),
+    date: new Date(2019, 9, 10),
     account: 0
 },
 {
     description: 'Netflix',
     amount: -12.99,
-    date: new Date(),
+    date: new Date(2019, 8, 25),
     account: 0
 }]
 
@@ -72,19 +72,13 @@ transfers = [{
     origin: 2,
     destination: 0,
     amount: 250,
-    date: new Date()
+    date: new Date(2019, 10, 1)
 },
 {
-    origin: 2,
-    destination: 0,
-    amount: 250,
-    date: new Date()
-},
-{
-    origin: 2,
-    destination: 0,
-    amount: 250,
-    date: new Date()
+    origin: 0,
+    destination: 3,
+    amount: 5000,
+    date: new Date(2019, 7, 22)
 }]
 
 module.exports = (() => ({
